f413dba6b08a6c122f4504f75214840038719674
Truong Vo Hoang Dung <truongvohoangdung1407@gmail.com>
2026-09-18 09:56:34 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Marxist-Leninist-philosophy-Learning-App/',
   plugins: [react(), tailwindcss()],
 })
